798a7191f3e6e06a986f3f7c0df26d273dee7d62
Blendo Costa <blendo.icosta@gmail.com>
2025-07-01 19:35:49 -0300

    fiz correções

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,10 @@
+// Script WhatsApp 
 function enviarWhatsApp(e) {
       e.preventDefault();
       const nome = document.getElementById("nome").value.trim();
       const telefone = document.getElementById("telefone").value.trim();
       const email = document.getElementById("email").value.trim();
       const numero = "5591984409973"; // Substitua pelo número real do WhatsApp
-      const mensagem = `Olá! Me chamo ${nome}, telefone: ${telefone}, e-mail: ${email}. Quero agendar minha avaliação e iniciar meus treinos.`;
+      const mensagem = `Olá! Me chamo ${nome} meu e-mail: ${email}. Quero agendar minha avaliação e iniciar meus treinos.`;
       window.open(`https://wa.me/${numero}?text=${encodeURIComponent(mensagem)}`, "_blank");
     }
