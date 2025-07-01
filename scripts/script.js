44def9696b0896fe6b644aaf10d5d7fb4a0adc44
Blendo Costa <blendo.icosta@gmail.com>
2025-07-01 19:43:58 -0300

    atualização

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 function enviarWhatsApp(e) {
       e.preventDefault();
       const nome = document.getElementById("nome").value.trim();
-      const telefone = document.getElementById("telefone").value.trim();
+      
       const email = document.getElementById("email").value.trim();
       const numero = "5591984409973"; // Substitua pelo número real do WhatsApp
       const mensagem = `Olá! Me chamo ${nome} meu e-mail: ${email}. Quero agendar minha avaliação e iniciar meus treinos.`;
